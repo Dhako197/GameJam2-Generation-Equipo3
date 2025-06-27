f173e8625fc0d2cef33bca13f3aad2ad685e03a3
Jonathan Flores <88913446+jonathanflores28@users.noreply.github.com>
2025-06-27 13:17:31 -0700

    Canvas UI
    pause menu and sounds

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public AudioSource pauseSound; // Opcional
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        if (pauseSound != null)
+            pauseSound.Play();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    
+}
